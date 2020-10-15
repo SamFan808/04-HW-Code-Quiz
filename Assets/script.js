757d31efd2395b3c8e870f6514ad3bdecc8f1e10
Sam Fan <samisfan@msn.com>
2020-10-14 21:07:28 -0600

    added pseudo code, bootstrap, jQuery links

---
+++ Assets/script.js
@@ -0,0 +1,22 @@
+// pesudo code
+
+// Base html page 
+
+// a START button
+//      gonna need a button
+//      event listener, listens for click
+//      Math.random question selector from an object/array with questions: and answers, will probably need an .each loop method to cycle through the object/array
+// TIMER starts
+//      gonna need a timer that countsdown from a set time (2:00 minutes? or 120 seconds)
+// A WILD QUESTION APPEARS
+//      some div field gets txt content updated
+//      some kind of input field - bootstrap
+//          if/else comparator validation to correct answer
+// correct answers, continues to next question
+//      something is keeping score, counter thing, maybe keep the counter near the timer
+// wrong answers, time is subtracted from the clock and continues to next question
+// game ends when either timer runs out, or all the questions have been answered
+// GAME OVER - score, and save initials in a high score page
+//      local storage for names and scores
+// HIGH SCORES - possibly a new page, otherwise it's a function that replaces the html on the page with a local storage list of high scores and user input names and scores
+
